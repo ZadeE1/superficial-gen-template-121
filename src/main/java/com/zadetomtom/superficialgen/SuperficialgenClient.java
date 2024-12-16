42c48c0c0e0cb9e0628ecfba2c977d292a5f727c
Zade <88864436+ZadeE1@users.noreply.github.com>
2024-12-16 17:39:34 +1100

    Initial commit

---
+++ src/main/java/com/zadetomtom/superficialgen/SuperficialgenClient.java
@@ -0,0 +1,10 @@
+package com.zadetomtom.superficialgen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SuperficialgenClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
